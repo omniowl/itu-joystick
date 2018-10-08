3df18949e79ea1820d388f879a63f7bf8f7d48b9
OmniOwl <xenon373@gmail.com>
2018-10-08 16:15:01 +0200

    Moved the Example Implementation to its own folder

---
+++ Assets/Source/Joypad/InputEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ITU.Joypad
+{
+    public class InputEventArgs : EventArgs
+    {
+        private KeyCode Key;
+
+        public InputEventArgs(KeyCode key)
+        {
+            this.Key = key;
+        }
+
+        public KeyCode GetKey()
+        {
+            return Key;
+        }
+    }
+}
